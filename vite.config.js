c11e2e12b8293ca58d32421746da7a861891a12d
limpiomatch <limpio.clean.match.app@gmail.com>
2026-09-19 15:20:37 -0500

    mejora para navegadores

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    target: "es2015", // Ayuda a la compatibilidad con navegadores in-app antiguos (WhatsApp, IG)
+  },
 });
